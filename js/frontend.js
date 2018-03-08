97a4d09c180dabec261e3716f2e46d987b8c5b23
gursharan1512 <sharanghotra1512@gmail.com>
2018-03-08 17:48:45 +0530

    removing hard coded path

--- js/frontend.js
+++ js/frontend.js
@@ -8,19 +8,19 @@ function getData(){
         var obj = JSON.parse(this.responseText);
         //getting the data from json file
         //details of standard document
-        var standardNouns = obj[0].nouns;
-        var standardAdjectives = obj[0].adjectives;
-        var standardVerbs = obj[0].verbs;
-        var standardWordCount = obj[0].wordCount;
+        var standardNouns = obj[0].standard_document_nouns;
+        var standardAdjectives = obj[0].standard_document_adjectives;
+        var standardVerbs = obj[0].standard_document_verbs;
+        var standardWordCount = obj[0].standard_document_wordCount;
         //details of eval document
-        var evalNouns = obj[1].nouns;
-        var evalAdjectives = obj[1].adjectives;
-        var evalVerbs = obj[1].verbs;
-        var evalWordCount = obj[1].wordCount;
+        var evalNouns = obj[1].evaluated_document_nouns;
+        var evalAdjectives = obj[1].evaluated_document_adjectives;
+        var evalVerbs = obj[1].evaluated_document_verbs;
+        var evalWordCount = obj[1].evaluated_document_wordCount;
         //commmon properties
-        var commonNouns = obj[2].nouns;
-        var commonAdjectives = obj[2].adjectives;
-        var commonVerbs = obj[2].verbs;
+        var commonNouns = obj[2].similar_nouns;
+        var commonAdjectives = obj[2].similar_adjectives;
+        var commonVerbs = obj[2].similar_verbs;
         var total = standardAdjectives+standardNouns+standardVerbs;
         var marks = commonNouns+commonVerbs+commonAdjectives;
         var percentage = Math.round((marks/total)*100);
--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 //fs node js .. taking up only .txt files and natural
 var fs = require('fs');
 var natural = require('natural');
+var path = require('path');
 var tokenizer = new natural.WordTokenizer();
 //pos tagger
-var path = require('path');
 var base_folder = path.join(path.dirname(require.resolve("natural")), "brill_pos_tagger");
 var rulesFilename = base_folder + "/data/English/tr_from_posjs.txt";
 var lexiconFilename = base_folder + "/data/English/lexicon_from_posjs.json";
@@ -12,7 +12,6 @@ var lexicon = new natural.Lexicon(lexiconFilename, defaultCategory);
 var rules = new natural.RuleSet(rulesFilename);
 var tagger = new natural.BrillPOSTagger(lexicon, rules);
 //pos tagger over
-//global variables to be displayed in json at end
 var evalNouns = [];
 var standardNouns = [];
 var evalAdjectives = [];
@@ -22,107 +21,75 @@ var standardVerbs = [];
 var similarNouns = [];
 var similarAdjectives = [];
 var similarVerbs = [];
+var standardFilePath = '../documents/standard.txt';
+var evalFilePath = '../documents/eval.txt';
 //reading the documents
-var evalDocument = fs.readFileSync('../documents/eval.txt','utf-8');
-var standardDocument = fs.readFileSync('../documents/standard.txt','utf-8');
+var evalDocument = fs.readFileSync(evalFilePath,'utf-8');
+var standardDocument = fs.readFileSync(standardFilePath,'utf-8');
 //tokenizing the documents
 var standardTokens = tokenizer.tokenize(standardDocument);
 var evalTokens = tokenizer.tokenize(evalDocument);
-//lengths of respective documents
 var standardLength = standardTokens.length;
 var evalLength = evalTokens.length;
-//twenty percent length relaxation
-var range = (standardTokens.length*20)/100
+var range = standardTokens.length*0.2;
 if(evalLength <= standardLength-range || evalLength >= standardLength+range){
   console.log("Document size invalid");
   return;
 }
 else{
   //call for calculating everything
-  calculate(standardTokens, evalTokens);
+  calculate(standardTokens,standardNouns,standardAdjectives,standardVerbs);
+  calculate(evalTokens,evalNouns,evalAdjectives,evalVerbs);
+  compare(standardNouns,evalNouns,similarNouns);
+  compare(standardAdjectives,evalAdjectives,similarAdjectives);
+  compare(standardVerbs,evalVerbs,similarVerbs);
+  writeJSON();
 }
 //function to count nouns, adjectives and verbs
-function calculate(standard, evaluate){
-  var standardTagged = tagger.tag(standard);
-  var evalTagged = tagger.tag(evaluate);
-  //for standard document
-  for(var i=0; i < standardTagged.length; i++){
-    if(standardTagged[i][1] == "NN"|"NNS"|"NNP"|"NNPS")
-      standardNouns.push(standardTagged[i][0]);
-    else if(standardTagged[i][1] == "JJ"|"JJR"|"JJS")
-      standardAdjectives.push(standardTagged[i][0]);
-    else if(standardTagged[i][1] == "VB"|"VBD"|"VBG"|"VBN"|"VBP")
-      standardVerbs.push(standardTagged[i][0]);
+function calculate(docToken,noun,adjective,verb){
+  var tagged = tagger.tag(docToken);
+  for(var i=0; i < tagged.length; i++){
+    if(tagged[i][1] == "NN"|"NNS"|"NNP"|"NNPS")
+      noun.push(tagged[i][0]);
+    else if(tagged[i][1] == "JJ"|"JJR"|"JJS")
+      adjective.push(tagged[i][0]);
+    else if(tagged[i][1] == "VB"|"VBD"|"VBG"|"VBN"|"VBP")
+      verb.push(tagged[i][0]);
   }
-    //for evaluation document
-  for(var i=0; i < evalTagged.length; i++){ 
-    if(evalTagged[i][1] == "NN"|"NNS"|"NNP"|"NNPS")
-      evalNouns.push(evalTagged[i][0]);
-    else if(evalTagged[i][1] == "JJ"|"JJR"|"JJS")
-      evalAdjectives.push(evalTagged[i][0]);
-    else if(evalTagged[i][1] == "VB"|"VBD"|"VBG"|"VBN"|"VBP")
-      evalVerbs.push(evalTagged[i][0]);
+}
+function compare(standard,evaluate,similar){
+  var spellcheck = new natural.Spellcheck(standard);
+  //comparing each noun, adjectives and verbs of evalDoc with corpus
+  for(let i = 0; i < evaluate.length; i++){
+  if(spellcheck.isCorrect(evaluate[i])){
+    similar.push(evaluate[i]);
   }
-   compareNouns();
-   compareAdjectives();
-   compareVerbs();
-   writeJSON();
-  }
-//comparing nouns of both documents and calculating noun percentage
-function compareNouns(){
-  var corpus = standardNouns;
-  var spellcheck = new natural.Spellcheck(corpus);
-  //comparing each noun of evalDoc with corpus
-  for(let i = 0; i < evalNouns.length; i++){
-	if(spellcheck.isCorrect(evalNouns[i])){
-		similarNouns.push(evalNouns[i]);
-	}
 }   
-} 
-function compareAdjectives(){
-  var corpus = standardAdjectives;
-  var spellcheck = new natural.Spellcheck(corpus);
-  //comparing each noun of evalDoc with corpus
-  for(let i = 0; i < evalAdjectives.length; i++){
-	if(spellcheck.isCorrect(evalAdjectives[i])){
-		similarAdjectives.push(evalAdjectives[i]);
-	}
-}   
-} 
-function compareVerbs(){
-  var corpus = standardVerbs;
-  var spellcheck = new natural.Spellcheck(corpus);
-  //comparing each noun of evalDoc with corpus
-  for(let i = 0; i < evalVerbs.length; i++){
-	if(spellcheck.isCorrect(evalVerbs[i])){
-		similarVerbs.push(evalVerbs[i]);
-	}
-} 
-} 
+}
 //function to make and write into JSON file
  function writeJSON(){
     var score = {
 "output":[
         {
-          wordCount : standardLength,
-          nouns : standardNouns.length,
-          adjectives : standardAdjectives.length,
-          verbs : standardVerbs.length
+          standard_document_wordCount : standardLength,
+          standard_document_nouns : standardNouns.length,
+          standard_document_adjectives : standardAdjectives.length,
+          standard_document_verbs : standardVerbs.length
         }, 
         {
-          wordCount : evalLength,
-          nouns : evalNouns.length,
-          adjectives : evalAdjectives.length,
-          verbs : evalVerbs.length
+          evaluated_document_wordCount : evalLength,
+          evaluated_document_nouns : evalNouns.length,
+          evaluated_document_adjectives : evalAdjectives.length,
+          evaluated_document_verbs : evalVerbs.length
         }, {
-          nouns : similarNouns.length,
-          adjectives : similarAdjectives.length,
-          verbs : similarVerbs.length
+          similar_nouns : similarNouns.length,
+          similar_adjectives : similarAdjectives.length,
+          similar_verbs : similarVerbs.length
       }
     ]
     }
   //object to string
   var json = JSON.stringify(score, null, 2);
   //writing in json file
-  fs.writeFileSync('result.json',json)
+  fs.writeFileSync('result.json',json);
  }
